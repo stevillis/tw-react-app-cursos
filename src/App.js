190d48d578dbcc45995b386667f20c1fd7f6f848
Stévillis Sousa <stevillis@hotmail.com>
2022-02-17 00:59:43 -0400

    Store courses on local database

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
+import { CourseService } from './services/CourseService';
+
 import Course from './components/Course';
 import NewCourseForm from './components/NewCourseForm';
 
@@ -13,24 +15,36 @@ class App extends Component {
     super(props);
 
     this.state = {
-      courses: []
+      courses: [
+
+      ]
     };
 
     this.add = this.add.bind(this);
     this.remove = this.remove.bind(this);
+    this.startData = this.startData.bind(this);
+
+    this.startData();
   }
 
-  add(course) {
+  async startData() {
+    const courses = await CourseService.list();
+    this.setState({ courses });
+  }
+
+  async add(course) {
     const { courses } = this.state,
-      newCourse = Object.assign({}, course, { id: (Date.now()) });
+      newCourse = await CourseService.create(course);
 
     courses.push(newCourse);
     this.setState({ courses });
   }
 
-  remove(courseId) {
+  async remove(courseId) {
     const { courses } = this.state,
       courseIndex = courses.findIndex(course => course.id === courseId);
+
+    await CourseService.remove(courseId);
 
     courses.splice(courseIndex, 1);
     this.setState({ courses });
